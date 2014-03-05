dbf77f04befd5e8579ac4671b8a414adfd349d4e
Dominic Steinitz <dominic@steinitz.org>
2014-03-05 09:25:57 +0000

    Now drawing a chart even if the legend is rubbish.

--- LinRegSingle.hs
+++ LinRegSingle.hs
@@ -9,9 +9,17 @@ import Data.Random.Source.PureMT
 import Data.Random
 import Control.Monad.State
 
+import System.Environment(getArgs)
+import Data.Colour.Names
+import Data.Colour
+import Control.Lens
+import Data.Default.Class
+import Data.Time.LocalTime
+import Graphics.Rendering.Chart
+import Graphics.Rendering.Chart.Backend.Cairo
 
 nSamples :: Int
-nSamples = 100
+nSamples = 10000
 
 beta :: Double
 beta = 2.0
@@ -28,6 +36,27 @@ testEpsilons m =
   evalState (replicateM m (sample StdNormal))
   (pureMT 2)
 
-testYs = V.zipWith (\x e -> beta * x + e) (testXs nSamples) (testEpsilons nSamples)
+testYs m = V.zipWith (\x e -> beta * x + e)
+           (testXs m) (testEpsilons m)
 
-main = putStrLn $ show testYs
+prices' :: [(Double,Double)]
+prices' = V.toList $ V.zip (testXs nSamples) (testYs nSamples)
+
+chart = toRenderable layout
+  where
+
+    price1 = plot_points_style . point_color .~ opaque red
+           $ plot_points_values .~ prices'
+           $ plot_points_title .~ "price 1"
+           $ def
+
+    layout = layoutlr_title .~"Price History"
+           $ layoutlr_left_axis . laxis_override .~ axisGridHide
+           $ layoutlr_right_axis . laxis_override .~ axisGridHide
+           $ layoutlr_x_axis . laxis_override .~ axisGridHide
+           $ layoutlr_plots .~ [Left (toPlot price1),
+                                Right (toPlot price1)]
+           $ layoutlr_grid_last .~ False
+           $ def
+
+main = renderableToFile def chart "example2_big.png"
